9bd8855015c8d6972a0819fcc27a5a5ee6c8c816
Wolfknightx <Wolfknightx@Gmail.com>
2016-06-04 15:35:47 -0500

    Board basic set up laid out.

--- TicTacToe2/Form1.cs
+++ TicTacToe2/Form1.cs
@@ -10,9 +10,9 @@ using System.Windows.Forms;
 
 namespace TicTacToe2
 {
-    public partial class Form1 : Form
+    public partial class TTTGameBoard : Form
     {
-        public Form1()
+        public TTTGameBoard()
         {
             InitializeComponent();
         }
