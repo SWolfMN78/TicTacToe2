5ead476688a71932bd444c3a926bbc99272e8a3c
Wolfknightx <Wolfknightx@Gmail.com>
2016-08-15 19:08:58 -0500

    started work on basic TTT game.

--- TicTacToe2/Form1.cs
+++ TicTacToe2/Form1.cs
@@ -16,5 +16,36 @@ namespace TicTacToe2
         {
             InitializeComponent();
         }
+
+        private void TTTGameBoard_Load(object sender, EventArgs e)
+        {
+            //main game board - here the application will call the methods needed to run.
+        }
+
+        private void btn1_Click(object sender, EventArgs e)
+        {
+            
+            
+        }
+
+        private void btnQuit_Click(object sender, EventArgs e)
+        {
+            //close the program once this is clicked.
+            Close();
+        }
+
+        private void btnRestart_Click(object sender, EventArgs e)
+        {
+            //when this button is pressed it will reset the game entirely.  If there is a running tracker it'll need to wipe that too.
+            btn1.Text = "";
+            btn2.Text = "";
+            btn3.Text = "";
+            btn4.Text = "";
+            btn5.Text = "";
+            btn6.Text = "";
+            btn7.Text = "";
+            btn8.Text = "";
+            btn9.Text = "";
+        }
     }
 }
